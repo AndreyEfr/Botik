9930ac07e7e9b18bfc5d1b2819cc394bd5d482a0
AndreyEfr <kabanefa@gmail.com>
2022-03-15 17:24:57 +0200

    add hist

--- account.py
+++ account.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from pprint import pprint
-
+import json
+import datetime
 TOKEN = '5189332828:AAFItsEfTsTLycNDcUfDHffpYAtSSCxpdQw'
 updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
@@ -11,6 +12,18 @@ def start(update, context):
         'operators are: +  -  *  /  //(integer division) %(remainder) **(degree)'
     context.bot.send_message(chat_id=chat.id, text=a)
 
+def calc_hist(s, b,action):
+    l = str(datetime.datetime.now())
+    memory_json =[
+    {
+        '1 numb':s,
+        '2 numb':b,
+        'action': action,
+        'time': l
+    }]
+    with open("hist.json","a")as f:
+        json.dump(memory_json, f, indent= 3)
+    
 
 def ca(s, b, action):
     if action == "+":
@@ -55,8 +68,10 @@ def ma(update, context):
         n = float(d)
     else:
         context.bot.send_message(chat_id=chat.id, text='invalid equation')
+    f = calc_hist(v, n, c)
     g = ca(v, n, c)
     context.bot.send_message(chat_id=chat.id, text=g)
+    context.bot.send_message(chat_id=chat.id, text=f)
 
 
 dispatcher.add_handler(CommandHandler('start', start))
